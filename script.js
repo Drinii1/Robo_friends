c49cf0f92b471fec6b4a2d838d2f57b140437160
Drin Pireci <drinpireci@hotmail.com>
2025-12-20 20:59:36 +0100

    avancing

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const citySelect = document.querySelector('#citySelect');
 const usersContainer = document.querySelector('#usersContainer');
 const statusText = document.querySelector('#statusText');
 
+const modalOverlay = document.getElementById('modalOverlay')
+
+const modal = document.getElementById('modal')
+
+const buttonModal = document.getElementById('closeModal')
+
+const modalContent = document.getElementById('modalContent')
+
 let users = [];
 
 const cities = [
@@ -37,6 +45,15 @@ const buildCityDropdown = () => {
   });
 };
 
+  const newAPI = async(userId) => {
+    const newResponse = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+    const newData = await newResponse.json()
+    newUsers = newData
+
+}
+
+  
+
 const renderUsers = (list) => {
   usersContainer.innerHTML = '';
 
@@ -51,27 +68,71 @@ const renderUsers = (list) => {
     `;
 
     usersContainer.appendChild(card);
+    card.addEventListener('click',() => {
+      modalContent.innerHTML = `<strong>Infos:</strong> <br>  <br> <strong>Email:</strong>${user.email} <br>
+     <strong> Username:</strong>${user.username}  <br> <strong>Address:</strong>${user.address.street} <br> <strong>Phone Number:</strong>${user.phone}  <br> <strong>Website</strong>:${user.website}
+      
+
+      `
+      
+            modalOverlay.style.display = 'flex'
+          
+            newAPI(user.id)
+    })
   });
 
   statusText.textContent = `Showing ${list.length} of ${users.length} users.`;
 };
 
+
+buttonModal.addEventListener('click', () => {
+    modalOverlay.style.display = 'none'
+
+});
+// const modalChange = () => {
+//     modal.innerHTML = modalOverlay
+//      buttonModal.innerHtml = modalOverlay
+
+//      modalContent.innerHTML = `Email:${user.email}`
+// }
+
+
+
+
+
+
+
 const applyFilters = () => {
   const searchValue = searchInput.value.toLowerCase();
   const selectedCity = citySelect.value;
 
-  const filteredUsers = users.filter(user => {
-    const matchesName = user.name.toLowerCase().includes(searchValue);
-    const matchesCity =
-      selectedCity === 'all' || user.address.city === selectedCity;
+  // const filteredUsers = users.filter(user => {
+  //   const matchesName = user.name.toLowerCase().includes(searchValue);
+  //   const matchesCity =
+  //     selectedCity === 'all' || user.address.city === selectedCity;
 
-    return matchesName && matchesCity;
-  });
+  //   return matchesName && matchesCity;
+  // });
+  let filteredUsers = users
+  if(searchInput){
+    filteredUsers = filteredUsers.filter(user => user.name.toLowerCase().includes(searchValue))
+  }
+  
+  
 
+  
+  if(selectedCity !== 'all' ){
+   
+      filteredUsers = filteredUsers.filter(user => user.address.city === selectedCity)
+  }
   renderUsers(filteredUsers);
 };
 
+
+
 searchInput.addEventListener('input', applyFilters);
 citySelect.addEventListener('change', applyFilters);
+
+
 
 loadUsers()
